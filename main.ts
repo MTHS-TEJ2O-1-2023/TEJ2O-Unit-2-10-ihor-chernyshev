77539c49c4964e30df929f074d9f115197229a3b
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-13 18:51:09 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -19,27 +19,39 @@ basic.showIcon(IconNames.Happy)
 
 basic.forever(function () {
   ll = input.lightLevel()
+  basic.showNumber(ll)
+  if (ll <= 51) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    basic.pause(500)
+  }
+  if (ll > 52) {
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.show()
+    basic.pause(500)
+    }
+  if (ll > 104) {
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    basic.pause(500)
+  }
+  if (ll > 156) {
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+    basic.pause(500)
+  }
+  if (ll > 208) {
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.show()
+    basic.pause(500)
+  }
 })
-if (ll <= 51){
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-  neopixelStrip.show()
-}
-if (ll > 52) {
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
-  neopixelStrip.show()
-}
-if (ll > 104) {
-  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
-  neopixelStrip.show()
-}
-if (ll > 156) {
-  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
-  neopixelStrip.show()
-}
-if (ll > 208) {
-  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
-  neopixelStrip.show()
-}
