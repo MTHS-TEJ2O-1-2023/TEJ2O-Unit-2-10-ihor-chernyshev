a2936d0245e17f271d6489238d29042315bfd4ab
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-15 16:00:00 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  *
  * Created by: Ihor Chernyshev
  * Created on: Oct 2023
- * This program ...
+ * This program uses neopixels on RobotBit
 */
 
 let ll: number = 0
@@ -26,7 +26,6 @@ basic.forever(function () {
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.show()
-    basic.pause(500)
   }
   if (ll > 52) {
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
@@ -34,24 +33,20 @@ basic.forever(function () {
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
     neopixelStrip.show()
-    basic.pause(500)
   }
   if (ll > 104) {
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
     neopixelStrip.show()
-    basic.pause(500)
   }
   if (ll > 156) {
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
     neopixelStrip.show()
-    basic.pause(500)
   }
   if (ll > 208) {
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
     neopixelStrip.show()
-    basic.pause(500)
   }
 })
