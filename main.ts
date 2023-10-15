9f971137480fe5e0dfbc0f382e1631a419b7ff59
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-15 16:46:39 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -18,8 +18,10 @@ neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
 basic.forever(function () {
+  // Getting the light level
   ll = input.lightLevel()
   basic.showNumber(ll)
+  // Turning on 0 neopixels
   if (ll <= 51) {
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
@@ -27,6 +29,7 @@ basic.forever(function () {
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.show()
   }
+  // Turning on 1 neopixel
   if (ll > 52) {
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
@@ -34,17 +37,20 @@ basic.forever(function () {
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
     neopixelStrip.show()
   }
+  // Turning on 2 neopixels
   if (ll > 104) {
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
     neopixelStrip.show()
   }
+  // Turning on 3 neopixels
   if (ll > 156) {
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
     neopixelStrip.show()
   }
+  // Turning on 4 neopixels
   if (ll > 208) {
     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
     neopixelStrip.show()
