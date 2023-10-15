c314c9482c91eb784567f431e9221d665f94d1b2
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-15 19:32:50 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -21,32 +21,32 @@ display.show(Image.HAPPY)
 while True:
     ll = display.read_light_level()
     display.scroll(display.read_light_level())
-    if (ll <= 51):
+    if ll <= 51:
         np[0] = (0, 0, 0)
         np[1] = (0, 0, 0)
         np[2] = (0, 0, 0)
         np[3] = (0, 0, 0)
         print(np[0], np[1], np[2], np[3])
         np.show()
-    if (ll > 52):
+    if ll > 52:
         np[1] = (0, 0, 0)
         np[2] = (0, 0, 0)
         np[3] = (0, 0, 0)
         np[0] = (255, 255, 255)
         print(np[0], np[1], np[2], np[3])
         np.show()
-    if (ll > 104):
+    if ll > 104:
         np[2] = (0, 0, 0)
         np[3] = (0, 0, 0)
         np[1] = (0, 255, 0)
         print(np[0], np[1], np[2], np[3])
         np.show()
-    if (ll > 156):
+    if ll > 156:
         np[3] = (0, 0, 0)
         np[2] = (255, 0, 0)
         print(np[0], np[1], np[2], np[3])
         np.show()
-    if (ll > 208):
+    if ll > 208:
         np[3] = (0, 0, 255)
         print(np[0], np[1], np[2], np[3])
         np.show()
